02cb8da1431a03215fa6bc5cc9ffce1b9d660861
NOP0 <33583294+NOP0@users.noreply.github.com>
2020-04-07 20:12:33 +0200

    Part 1 Finished

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-#[derive(PartialEq)]
+use std::io::{Write, stdin, stdout};
+
+#[derive(PartialEq, Clone, Copy)]
 enum Token{
     Integer(i32),
-    Plus(String),
+    Plus,
     EOF,
+}
+
+impl Token{
+    fn variant_eq(left: Token, right: Token) -> bool {
+        use Token::*;
+        match(left, right) {
+            (Integer(_), Integer(_)) => true,
+            (Plus, Plus) => true,
+            (EOF, EOF) => true,
+            (_,_) => false,
+        }
+    }
 }
 
 
@@ -13,8 +27,19 @@ struct Interpreter {
 }
 
 impl Interpreter {
-    fn error(self) {
-        panic!("Could not parse input");
+
+    fn new(text: String) -> Interpreter {
+        Interpreter {
+            text,
+            pos : 0,
+            current_token : Token::EOF, 
+        }
+    }
+
+    fn reload(&mut self, text: String) {
+        self.text = text;
+        self.pos = 0;
+        self.current_token = Token::EOF; 
     }
 
     fn get_next_token(&mut self) -> Result<Token, ()> {
@@ -27,11 +52,11 @@ impl Interpreter {
         let current_char : char = self.text.as_bytes()[self.pos] as char;
             
         if current_char.is_ascii_digit() {
-            token = Token::Integer(current_char as i32);
+            token = Token::Integer(current_char.to_digit(10).unwrap() as i32);
             self.pos += 1;
         }
         else if current_char == '+' {
-            token = Token::Plus("+".to_string());
+            token = Token::Plus;
             self.pos += 1;
         }
         else {
@@ -43,12 +68,52 @@ impl Interpreter {
     }
 
     fn eat(&mut self, token: Token) {
-        if self.current_token == token {
+        if Token::variant_eq(token, self.current_token) {
             self.current_token = self.get_next_token().expect("Incorrect token");
         }
+        else {
+            panic!("Wrong token")
+        }
+    }
+
+    fn expr(&mut self) -> i32 {
+
+        self.current_token = self.get_next_token().unwrap(); // Get first token
+
+        let left = self.current_token; // Save left operand
+        self.eat(Token::Integer(0)); // Should be integer
+
+        let op = self.current_token; // Save operator
+        self.eat(Token::Plus); // Should be PLUS
+
+        let right = self.current_token; // Save right operand
+        self.eat(Token::Integer(0)); // Should be integer 
+
+        
+        if let (Token::Integer(left_value), Token::Integer(right_value)) = (left, right){
+            left_value + right_value
+        }  
+        else
+        {
+            panic!()
+        }
+
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()>{
+    let mut text : String = "".to_string();
+    let mut interpreter = Interpreter::new(text.clone());
+    loop{
+        print!(">>");
+        stdout().flush()?;
+        
+        text.clear();
+        stdin().read_line(&mut text)?;
+        text = text.trim().to_string();
+        interpreter.reload(text.clone());
+        let result = interpreter.expr();
+        println!("{}", result);
+        
+    }
 }
