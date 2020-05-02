cac092d46230e92582978a1e93507d0c700019a2
NOP0 <33583294+NOP0@users.noreply.github.com>
2020-05-02 22:04:39 +0200

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use std::io::{Write, stdin, stdout};
+use std::io::{stdin, stdout, Write};
 
 #[derive(PartialEq, Clone, Debug)]
-enum Token{
+enum Token {
     Integer(i32),
     Plus,
     Minus,
     Mul,
     Div,
     Rparen,
-    Lparen
+    Lparen,
 }
 
-impl Token{
+impl Token {
     fn variant_eq(left: Token, right: &Token) -> bool {
         use Token::*;
-        match(left, right) {
+        match (left, right) {
             (Integer(_), Integer(_)) => true,
             (Plus, Plus) => true,
             (Minus, Minus) => true,
@@ -22,26 +22,23 @@ impl Token{
             (Div, Div) => true,
             (Rparen, Rparen) => true,
             (Lparen, Lparen) => true,
-            (_,_) => false,
+            (_, _) => false,
         }
     }
 }
 
-
-
 struct Lexer {
-    text : Vec<char>,
-    pos : usize,
-    current_char : Option<char>,
+    text: Vec<char>,
+    pos: usize,
+    current_char: Option<char>,
 }
 
 impl Lexer {
-
     fn new(text: String) -> Lexer {
         Lexer {
             text: text.chars().collect(),
-            pos : 0,
-            current_char : text.chars().nth(0),
+            pos: 0,
+            current_char: text.chars().nth(0),
         }
     }
 
@@ -49,8 +46,7 @@ impl Lexer {
         self.pos += 1;
         if self.pos > self.text.len() - 1 {
             self.current_char = None;
-        }
-        else {
+        } else {
             self.current_char = Some(self.text[self.pos]);
         }
     }
@@ -59,8 +55,7 @@ impl Lexer {
         while let Some(ch) = self.current_char {
             if ch.is_whitespace() {
                 self.advance()
-            }
-            else {
+            } else {
                 break;
             }
         }
@@ -72,57 +67,47 @@ impl Lexer {
             if ch.is_digit(10) {
                 result.push(ch);
                 self.advance();
+            } else {
+                break;
             }
-            else {break;}
         }
         result.parse().unwrap()
     }
 
     fn get_next_token(&mut self) -> Option<Token> {
-
-        let mut token : Option<Token> = None;
+        let mut token: Option<Token> = None;
         while let Some(ch) = self.current_char {
             if ch.is_whitespace() {
                 self.skip_whitespace();
                 continue;
-            }
-            else if ch.is_digit(10) {
+            } else if ch.is_digit(10) {
                 token = Some(Token::Integer(self.integer()));
                 break;
-            }
-            else if ch =='+' {
+            } else if ch == '+' {
                 self.advance();
                 token = Some(Token::Plus);
                 break;
-            }
-            else if ch == '-' {
+            } else if ch == '-' {
                 self.advance();
                 token = Some(Token::Minus);
                 break;
-            }
-
-            else if ch == '*' {
+            } else if ch == '*' {
                 self.advance();
                 token = Some(Token::Mul);
                 break;
-            }
-
-            else if ch == '/' {
+            } else if ch == '/' {
                 self.advance();
                 token = Some(Token::Div);
                 break;
-            }
-            else if ch == '(' {
+            } else if ch == '(' {
                 self.advance();
                 token = Some(Token::Lparen);
                 break;
-            }
-            else if ch == ')' {
+            } else if ch == ')' {
                 self.advance();
                 token = Some(Token::Rparen);
                 break;
-            }
-            else {
+            } else {
                 panic!();
             }
         }
@@ -131,10 +116,10 @@ impl Lexer {
 }
 
 struct Interpreter {
-    lexer : Lexer,
+    lexer: Lexer,
     current_token: Token,
 }
-    
+
 impl Interpreter {
     fn new(mut lexer: Lexer) -> Interpreter {
         let current_token = lexer.get_next_token().expect("No first token");
@@ -149,65 +134,58 @@ impl Interpreter {
             if let Some(token) = self.lexer.get_next_token() {
                 self.current_token = token;
             }
-        }
-        else {
-            panic!("Wrong token, got {:?}, expected {:?}", self.current_token, token);
+        } else {
+            panic!(
+                "Wrong token, got {:?}, expected {:?}",
+                self.current_token, token
+            );
         }
     }
 
     fn factor(&mut self) -> i32 {
-        let result : i32;
+        let result: i32;
         let token = self.current_token.clone();
 
         match token {
-            Token::Integer(value) => 
-                {
-                    self.eat(Token::Integer(0));
-                    result = value;
-                }
-            Token::Lparen =>
-                {
-                    self.eat(Token::Lparen);
-                    result = self.expr();
-                    self.eat(Token::Rparen);
-                }
-            _ => panic!("Unexpected token in factor")
-
-    }
+            Token::Integer(value) => {
+                self.eat(Token::Integer(0));
+                result = value;
+            }
+            Token::Lparen => {
+                self.eat(Token::Lparen);
+                result = self.expr();
+                self.eat(Token::Rparen);
+            }
+            _ => panic!("Unexpected token in factor"),
+        }
         result
     }
 
     fn term(&mut self) -> i32 {
         let mut result = self.factor();
 
-         while 
-        (self.current_token == Token::Mul)
-        |(self.current_token == Token::Div)  {
+        while (self.current_token == Token::Mul) | (self.current_token == Token::Div) {
             match self.current_token {
                 Token::Mul => {
                     self.eat(Token::Mul);
-                    result = result*self.factor();
+                    result = result * self.factor();
                 }
                 Token::Div => {
                     self.eat(Token::Div);
-                    result = result/self.factor();
+                    result = result / self.factor();
                 }
-                _ => panic!()
+                _ => panic!(),
             }
         }
         result
-        
     }
 
     fn expr(&mut self) -> i32 {
-
         let mut result;
 
         result = self.term();
 
-        while 
-        (self.current_token == Token::Plus)
-        |(self.current_token == Token::Minus) {
+        while (self.current_token == Token::Plus) | (self.current_token == Token::Minus) {
             match self.current_token {
                 Token::Plus => {
                     self.eat(Token::Plus);
@@ -217,16 +195,16 @@ impl Interpreter {
                     self.eat(Token::Minus);
                     result = result - self.term();
                 }
-                _ => panic!()
+                _ => panic!(),
             }
         }
         result
     }
 }
 
-fn main() -> std::io::Result<()>{
-    loop{
-        let mut text : String = "".to_string();
+fn main() -> std::io::Result<()> {
+    loop {
+        let mut text: String = "".to_string();
         print!(">>");
         stdout().flush()?;
         text.clear();
@@ -236,6 +214,5 @@ fn main() -> std::io::Result<()>{
         let mut interpreter = Interpreter::new(lexer);
         let result = interpreter.expr();
         println!("{}", result);
-        
     }
 }
