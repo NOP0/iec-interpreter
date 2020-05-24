4879051b25a519bff537a82fcab1c4eb2fae85d5
NOP0 <33583294+NOP0@users.noreply.github.com>
2020-05-24 21:51:24 +0200

    Argument parsing. Tests folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{stdin, stdout, Write};
-use log::*;
+use std::{fs, env};
 
 mod ast;
 mod interpreter;
@@ -12,18 +12,37 @@ use lexer::Lexer;
 use parser::Parser;
 
 fn main() -> std::io::Result<()> {
-    loop {
-        let mut text: String = "".to_string();
-        print!(">>");
-        stdout().flush()?;
-        text.clear();
-        stdin().read_line(&mut text)?;
-        text = text.trim().to_string();
-        let lexer = Lexer::new(text.clone());
-        let parser = Parser::new(lexer);
-        let mut interpreter = Interpreter::new(parser);
-        interpreter.interpret();
+
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {  // No args 
+            loop {
+                let mut text: String = "".to_string();
+                print!(">>");
+                stdout().flush()?;
+                text.clear();
+                stdin().read_line(&mut text)?;
+                text = text.trim().to_string();
+                let lexer = Lexer::new(text.clone());
+                let parser = Parser::new(lexer);
+                let mut interpreter = Interpreter::new(parser);
+                interpreter.interpret();
+            }
+          
+        }
+        2 => { // Program argument
+            let path = std::path::PathBuf::from(args[1].clone());
+            let text = fs::read_to_string(path.clone()).expect(&format!("Could not open file {:?}", path));
+           // let text = fs::read_to_string("../../tests/addition.st").expect("Could not open file");
+            let lexer = Lexer::new(text);
+            let parser = Parser::new(lexer);
+            let mut interpreter = Interpreter::new(parser);
+            interpreter.interpret();
+        }
+        _ => {println!("Usage: 1 program file argument or no argument for REPL");}
     }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -44,7 +63,6 @@ fn interpret_addition() {
 #[test]
 fn interpret_program() {
     let _ = env_logger::builder().is_test(true).try_init();
-    info!("Test");
     let text = 
     "PROGRAM
         1+2
